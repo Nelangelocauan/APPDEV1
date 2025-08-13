29410a16b8569fc96c74023278926af5d4dc6d8f
x <x@ca01.ub.lan>
2025-08-13 13:18:06 +0800

    I Added a Counter

---
+++ Counter/script.js
@@ -0,0 +1,39 @@
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+const decrementButton = document.getElementById('decrement');
+let count=0
+
+btns.forEach((btn) => {
+    btn.addEventListener('click',(button) => {
+        const styles = button.currentTarget.classList
+        if(styles.contains('random')){
+            getRandomNum()
+            console.log('click')
+        }else if (styles.contains('increment')){
+             count++
+        
+        }else if (styles.contains('decrement')){
+             count--  
+        }else if (styles.contains('reset'))
+            count = 0
+        value.textContent = count
+
+        if (count>0){
+            value.style.color = "green"
+        }else if (count <0){
+            value.style.color = "red"
+        }
+    })
+})
+
+
+getRandomNum = ()=> {
+    if (count > 0){
+        count = (Math.floor(Math.random()* 100)) -1
+    } else if (count <0){
+        count = (Math.floor(Math.random()* 100))
+    } else {
+        count = Math.floor(Math.random()*100)
+    }
+}
+
